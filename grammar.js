2ba06b0150bebf8671ed307e8633516cce21bcff
Benjamin <benjaminstandfield@gmail.com>
2025-08-30 14:40:37 -0400

    perf: handle unrestricted names in safer regex

--- grammar.js
+++ grammar.js
@@ -18,7 +18,7 @@ module.exports = grammar({
     // unrestricted_names are single quoted strings that can contain any
     // character (not really but kinda...).
     basic_name: (_) => /[A-Za-z_][A-Za-z0-9_]*/,
-    unrestricted_name: (_) => seq("'", /.*/, "'"),
+    unrestricted_name: (_) => /'([^'\\]|\\.)*'/,
 
     // names or short names will appear in Sysmlv2 text
     short_name: ($) => seq("<", choice($.basic_name, $.unrestricted_name), ">"),
